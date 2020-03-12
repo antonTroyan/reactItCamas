5fdb5efc653ecd659a42fcfd95b7e24e7abd74e9
Anton_Trayan <anton.troyan@gmail.com>
2020-03-12 11:38:15 +0300

    lesson 11

--- src/App.js
+++ src/App.js
@@ -1,15 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
-
 
 const App = () => {
     return (
-        <div className="App">
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+
+            <header className='header'>
+                <img src='https://i.pinimg.com/236x/19/25/b7/1925b72b3e8d8df8fa22b6bff9052642.jpg'/>
+            </header> 
+
+            <nav className='nav'>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
+            </nav>
+
+            <div className='content'>
+                <div> 
+                    <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg'/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    my posts
+                    <div>new post</div>
+                    <div>post1</div>
+                    <div>post2</div>
+                </div>
+            </div>
         </div>
     );
 }
