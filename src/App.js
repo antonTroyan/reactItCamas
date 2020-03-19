1281b9bdc9daa5b5759160e5dcb1ba1fe092feeb
Anton_Trayan <anton.troyan@gmail.com>
2020-03-19 18:00:09 +0300

    Lesson 18

--- src/App.js
+++ src/App.js
@@ -3,13 +3,18 @@ import './App.css';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Profile from './components/profile/Profile';
+import Dialogs from './components/dialogs/Dialogs';
+
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/* <Profile/> */}
+            <div className='app-wrapper-content'>
+                <Dialogs />
+            </div>
         </div>
     );
 }
--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg' />
         </div>
