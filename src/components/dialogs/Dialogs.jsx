dfab7df7247a5d61365f8e2488c4b5ff7872c20a
Anton_Trayan <anton.troyan@gmail.com>
2020-03-20 14:19:48 +0300

    Lesson 21.

--- src/components/dialogs/Dialogs.jsx
+++ src/components/dialogs/Dialogs.jsx
@@ -3,8 +3,36 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialogs + ' ' + s.active}>
+                    Dimich
+                </div>
+                <div className={s.dialogs}>
+                    Andrey
+                </div>
+                <div className={s.dialogs}>
+                    Ivan
+                </div>
+                <div className={s.dialogs}>
+                    Ivan
+                </div>
+                <div className={s.dialogs}>
+                    Ivan
+                </div>
+                <div className={s.dialogs}>
+                    Ivan
+                </div>
+                <div className={s.dialogs}>
+                    Ivan
+                </div>
+                
+            </div>
+            <div className={s.messages}>
+                <div className={s.dialog}>Hi</div>
+                <div className={s.dialog}>How are you</div>
+                <div className={s.dialog}>Yo</div>
+            </div>
         </div>
     )
 }
