fad1c722c98a81e03a5686d935f359c5106cdc63
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2020-05-03 22:21:30 +0300

    Refactoring. Home work.

--- src/components/profile/myposts/profileInfo/ProfileInfo.jsx
+++ src/components/profile/myposts/profileInfo/ProfileInfo.jsx
@@ -6,6 +6,7 @@ const ProfileInfo = (props) => {
     if (!props.profile){
         return <Preloader/>
     }
+    debugger
 
     return (
         <div>
@@ -13,9 +14,18 @@ const ProfileInfo = (props) => {
                 <img src='https://image.shutterstock.com/image-photo/bright-spring-view-cameo-island-260nw-1048185397.jpg' />
             </div>
             <div className={s.descriptionBlock}>
-
                 <img src={props.profile.photos.large}/>
-                ava + description
+                <div>About me -  {props.profile.aboutMe}</div>
+                <div>Facebook -  {props.profile.contacts.facebook}</div>
+                <div>WebSite  -  {props.profile.contacts.website}</div>
+                <div>Vk       -  {props.profile.contacts.vk}</div>
+                <div>Twitter  -  {props.profile.contacts.twitter}</div>
+                <div>Instagram-  {props.profile.contacts.instagram}</div>
+                <div>Youtube  -  {props.profile.contacts.youtube}</div>
+                <div>GitHub   -  {props.profile.contacts.github}</div>
+
+                <div>Looking for a job - {props.profile.lookingForAJob}</div>
+                <div>Looking for a job desc - {props.profile.lookingForAJobDescription}</div>
             </div>
         </div>
     )
