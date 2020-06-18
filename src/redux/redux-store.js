6a8e4d22890c72c4b9be38b513fb7aa6283101f2
Anton_Trayan <anton.troyan@gmail.com>
2020-06-18 17:27:06 +0300

    Lesson 91 - chrome extensions для react и redux - React JS.

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers, applyMiddleware} from "redux";
+import {createStore, combineReducers, applyMiddleware, compose} from "redux";
 import profileReducer from "./profile-reducer";
 import dialogsReducer from "./dialogs-reducer";
 import sidebarReducer from "./sidebar-reducer";
@@ -20,8 +20,8 @@ let reducers = combineReducers({
 });
 
 // applyMiddleware() add new logic layer
-let store = createStore(reducers, applyMiddleware(thunkMiddleware));
-
-window.store = store;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; --- [integration with chrome extension]
+ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+ const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store;
