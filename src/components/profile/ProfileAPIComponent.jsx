49166ff906f424d5efc8d6631c76c386578090d8
antonTroyan <32644076+antonTroyan@users.noreply.github.com>
2020-06-25 19:44:26 +0300

    Lesson 97 ReactJS - обновление профиля.

--- src/components/profile/ProfileAPIComponent.jsx
+++ src/components/profile/ProfileAPIComponent.jsx
@@ -4,6 +4,16 @@ import Profile from "./Profile";
 class ProfileAPIComponent extends React.Component {
 
     componentDidMount() {
+        this.refreshProfile();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId !== prevProps.match.params.userId) {
+            this.refreshProfile();
+        }
+    }
+
+    refreshProfile() {
         let userId = this.props.match.params.userId;
         if (!userId) {
             userId = this.props.authorizedUserId;
@@ -16,9 +26,11 @@ class ProfileAPIComponent extends React.Component {
         /// {...this.props} open props and send them
         /// as attributes [pass all attributes that come to container]
         return <Profile {...this.props}
-            profile={this.props.profile}
-            status={this.props.status}
-            updateStatus={this.props.updateUserStatusThunkCreator}
+                        isOwner={!this.props.match.params.userId}
+                        profile={this.props.profile}
+                        status={this.props.status}
+                        updateStatus={this.props.updateUserStatusThunkCreator}
+                        savePhoto={this.props.savePhoto}
         />
     }
 }
--- src/components/profile/ProfileContainer.jsx
+++ src/components/profile/ProfileContainer.jsx
@@ -1,21 +1,22 @@
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import ProfileAPIComponent from "./ProfileAPIComponent";
 import {
     getUserProfileThunkCreator,
     getUsersStatusThunkCreator,
+    savePhotoThunkCreator,
     updateUserStatusThunkCreator
 } from "../../redux/profile-reducer";
-import { withRouter } from "react-router-dom";
-import { withAuthRedirect } from '../../hoc/withAuthRedirect';
-import { compose } from 'redux';
+import {withRouter} from "react-router-dom";
+import {withAuthRedirect} from '../../hoc/withAuthRedirect';
+import {compose} from 'redux';
 
 
 let mapStateToProps = (state) => {
     return {
-        profile          : state.profilePage.profile,
-        status           : state.profilePage.status,
-        authorizedUserId : state.authReducer.userId,
-        isAuth           : state.authReducer.isAuth
+        profile: state.profilePage.profile,
+        status: state.profilePage.status,
+        authorizedUserId: state.authReducer.userId,
+        isAuth: state.authReducer.isAuth
     }
 };
 
@@ -23,9 +24,10 @@ let mapStateToProps = (state) => {
 // this function aggregates wrappers
 export default compose(
     connect(mapStateToProps, {
-        getUserProfileThunkCreator   : getUserProfileThunkCreator,
-        getUserStatusThunkCreator    : getUsersStatusThunkCreator,
-        updateUserStatusThunkCreator : updateUserStatusThunkCreator
+        getUserProfileThunkCreator: getUserProfileThunkCreator,
+        getUserStatusThunkCreator: getUsersStatusThunkCreator,
+        updateUserStatusThunkCreator: updateUserStatusThunkCreator,
+        savePhoto: savePhotoThunkCreator,
     }),
     // wrapper that add info about url to ProfileAPIComponent
     withRouter,
